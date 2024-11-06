19e68ab510780d9852a769a3b5dbe4582509a34f
luis229822 <luis22199822@gmail.com>
2024-11-06 11:45:59 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Alonso Cueto y su opinión sobre la literatura de Abdulrazak Gurnah",
+"autor" => "Alonso Cueto",
+"resumen" => "A propósito de la llegada al Perú de Abdulrazak Gurnah, ganador del Premio Nobel de Literatura, el escritor peruano Alonso Cueto repasa su obra.",
 ];
 ?>
