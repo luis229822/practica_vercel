b5cc8c4892761e35b530f0d2bf5af661321ed525
luis229822 <luis22199822@gmail.com>
2024-11-06 11:35:33 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Caja Piura y su estrategia en el crédito: “Hoy tenemos la posibilidad de competir por tasa”",
+"autor" => "Israel Lozano Girón",
+"resumen" => "En entrevista, Bereche revela que la entidad microfinanciera ingresará al segmento de créditos y transformará el perfil de su tarjeta de crédito",
 ];
 ?>
