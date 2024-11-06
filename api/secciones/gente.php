e510a330eb9a16670726fcea37d62f352965cc6b
luis229822 <luis22199822@gmail.com>
2024-11-06 11:39:06 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "¿Cuánto dinero deben invertir los padres para que su hijo sea futbolista profesional?",
+"autor" => "Julio Vizcarra Torres",
+"resumen" => "Distintos factores determinarán que un niño llegue al fútbol profesional, más allá del talento que siempre será necesario. Los recursos económicos de los padres también serán claves en busca de hacer realidad el sueño. En esta nota intentamos explicar cuál sería la ruta a seguir y todo lo que se requiere para llegar al final del camino.",
 ];
 ?>
