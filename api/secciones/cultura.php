2539906bbf9148eb67a3992ad64162eca5f4746f
luis229822 <luis22199822@gmail.com>
2024-11-06 11:34:37 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Estación La Cultura cerrará durante Cumbre APEC Perú 2024",
+"autor" => "wwww.elcomercio.pe/",
+"resumen" => "Este cierre está programado para los días miércoles 13, jueves 14, viernes 15 y sábado 16 de noviembre.",
 ];
 ?>
