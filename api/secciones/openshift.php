de403b7a33eb5bbefe7f21dabbb0c3f7ca404d69
luis229822 <luis22199822@gmail.com>
2024-11-06 11:43:41 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Openshift AI y RHEL AI: sinergia entre inteligencia artificial y código abierto",
+"autor" => "Victor Chavez",
+"resumen" => "Es el encuentro donde damos a conocer las nuevas tecnologías en las que estamos trabajando. Son dos los lanzamientos principales: el RHEL AI y Openshift AI. Se trata conectar puntos: IA, modernización de aplicaciones, la nube híbrida abierta, etcétera. Porque es ahí cuando comienza a hacer sentido el valor de Red Hat”, comentó en entrevista Cordero.",
 ];
 ?>
