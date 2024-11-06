7d0564e1fd474c8dc1da8712a5adcbb73fc550da
luis229822 <luis22199822@gmail.com>
2024-11-06 11:48:52 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "América TV presenta “América Multimedia” y amplía su oferta digital e internacional",
+"autor" => "Redacción EC",
+"resumen" => "El canal avanzó en su transformación digital con el lanzamiento de “América Multimedia”, plataforma que incluye nuevos contenidos exclusivos",
 ];
 ?>
