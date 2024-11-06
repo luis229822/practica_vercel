8a26a3ea10f0d5719628736667fba4e14b6354f9
luis229822 <luis22199822@gmail.com>
2024-11-06 11:47:59 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Nvidia supera a Apple como la compañía más valiosa del mundo, en pleno auge de la IA",
+"autor" => "Agencia AFP",
+"resumen" => "La empresa de semiconductores gana terreno en plena era de la inteligencia artificial (IA) generativa. Sigue sumando más inversiones",
 ];
 ?>
