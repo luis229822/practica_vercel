6612b3068b55be76cdf812fab00c7bb1efc94557
luis229822 <luis22199822@gmail.com>
2024-11-06 11:49:34 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El Agustino: Joven es arrastrada por ladrones y sufre robo de S/350 en violento asalto",
+"autor" => "Redacción EC",
+"resumen" => "La inseguridad ciudadana en El Agustino sigue en aumento. Una joven y su madre fueron víctimas de un asalto en la calle Manuel Fumagalli. A pesar de la resistencia de las mujeres, los delincuentes huyeron con S/350 de sus víctimas. (Video: América TV)",
 ];
 ?>
