9281d1fc823db476f050c8fdeaecba5515f1781f
luis229822 <luis22199822@gmail.com>
2024-11-06 11:41:31 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Valencia lucha para recuperarse tras inundaciones que dejan más de 150 muertos",
+"autor" => "Christian Mestanza Arquiñigo",
+"resumen" => "Las torrenciales lluvias desbordaron ríos, arrasaron autos y viviendas y dejaron a cientos de personas aisladas en Valencia, España. El Comercio conversó con dos residentes atrapados en medio del caos",
 ];
 ?>
