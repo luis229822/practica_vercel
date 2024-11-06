1bf3d7bef05f36be10e2ee38efc40675cf08d57f
luis229822 <luis22199822@gmail.com>
2024-11-06 11:47:19 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "¿Cuándo cae el próximo feriado del 2024 Perú? Conoce los festivos y próximas fechas libres",
+"autor" => "Redacción EC",
+"resumen" => "Consulta el calendario peruano oficial con feriados, festivos y días no laborables del 2024.",
 ];
 ?>
