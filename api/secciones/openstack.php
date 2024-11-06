73792daa4583fd5d4a299a1781f75b1f60939f2c
luis229822 <luis22199822@gmail.com>
2024-11-06 11:44:59 -0500

    Update openstack.php

--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "NoticiasOpenStack: todo listo para dar servicio a las empresas que dejan atrás a VMware",
+"autor" => " Celia Valdeolmillos ",
+"resumen" => "La subida de precios y los cambios en las condiciones que están experimentando no pocos clientes de VMware después de que Broadcom la comprase han provocado mucho malestar en ellos, y un goteo de salidas de usuarios en busca de mejores condiciones y precios.",
 ];
 ?>
