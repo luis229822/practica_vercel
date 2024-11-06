d7576140f105c9bd8f9038870b5c142b98ff0b50
luis229822 <luis22199822@gmail.com>
2024-11-06 11:36:18 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "¿El sueldo mínimo aumentará este 2024 en Perú? | Lo que se acordó desde el CNTPE, posturas y más",
+"autor" => "Daniela Rodas Ronceros",
+"resumen" => "En la última sesión del Consejo Nacional del Trabajo y Promoción del Empleo (CNTPE) aumentó la incertidumbre sobre el incremento de sueldo mínimo en Perú. Conoce aquí los últimos pronunciamientos al respecto.",
 ];
 ?>
