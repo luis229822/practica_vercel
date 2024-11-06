04044bc156c134703372ae4b9a46aec24ac525be
luis229822 <luis22199822@gmail.com>
2024-11-06 11:37:52 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+"titulo" => "EsSalud: personal de Padomi brinda capacitación en cuidados paliativos",
+"autor" => "Redacción EC",
+"resumen" => "Este evento incluyó un completo programa de capacitación académica, científica y de gestión en cuidados paliativos.",
 ];
 ?>
