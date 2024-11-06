cc2ab7dd16f882d724743eb1563974ca9870d36d
luis229822 <luis22199822@gmail.com>
2024-11-06 11:30:50 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -1,11 +1,11 @@
-<?php
+La ciencia latinoamericana se une contra la pérdida de biodiversidad<?php
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La ciencia latinoamericana se une contra la pérdida de biodiversidad",
+"autor" => "www.caf.com",
+"resumen" => "En la antesala de la COP16 de Cali, las instituciones científicas de América Latina y el Caribe deben elevar su voz, hacer llegar su conocimiento sobre la pérdida de la biodiversidad e identificar prioridades de investigación para traducirlas en acciones concretas. Estos temas se tratarán en un evento de CAF el 28 de agosto en el Parque del Chicó de Bogotá.",
 ];
 ?>
