b7ca7511855ef674455f3268cfc3f4e10f1ca133
luis229822 <luis22199822@gmail.com>
2024-11-06 11:40:38 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Linus Torvalds: el revolucionario detrás de Linux y Git",
+"autor" => " Revista Mercado",
+"resumen" => "Linus Torvalds es el genio finlandés que transformó la tecnología moderna al crear Linux, el sistema operativo de código abierto que impulsa millones de servidores, smartphones (gracias a Android) y supercomputadoras en todo el mundo",
 ];
 ?>
