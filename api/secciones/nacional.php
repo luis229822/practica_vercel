5549a2a9147a63f9fdfbcb8786a89fd46e777022
luis229822 <luis22199822@gmail.com>
2024-11-06 11:42:19 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Pago de la CTS, noviembre 2024: cuándo es, cómo calcularla y cómo disponer de este dinero",
+"autor" => "Redacción EC",
+"resumen" => "Conoce todos los detalles del depósito de CTS que se realizará en este undécimo mes del año.",
 ];
 ?>
