b41e38c7e4543c0f027ce643bbad7f991eb6dde9
luis229822 <luis22199822@gmail.com>
2024-11-06 11:32:46 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Tráfico en canal de Suez cae por debajo del nivel de crisis del “Ever Given”",
+"autor" => "https://gestion.pe/",
+"resumen" => "Hace justo un año, la media de buques que atravesó el canal en la semana móvil que culmino el 14 de enero fue de 70 barcos entre mercantes y petroleros. Hoy la situación es muy diferente.",
 ];
 ?>
